88676f71132f8615d8c90db0e12fcc191d1cba7e
Erickson Teixeira <erickson.teixeira@linkka.com.br>
2018-10-15 14:16:01 -0300

    validation part1

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+//load input validation
+const validadeRegisterInput = require('../../validation/register');
+
 //load user model
 const User = require('../../models/User');
 
@@ -18,12 +21,19 @@ router.get('/test', (req, res) => res.json(
 //@route GET api/users/register
 //@description Register user
 //@access public
-
 router.post('/register', (req, res)=> {
+    const {errors, isValid} = validadeRegisterInput(req.body);
+
+    //check validation
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email : req.body.email })
     .then(user => {
         if(user){
-            return res.status(400).json({email : "Email already exists"});
+            errors.email = "Email already exists";
+            return res.status(400).json(errors);
         } else {
             const avatar = gravatar.url(req.body.email,{
                 s: '200', // size
