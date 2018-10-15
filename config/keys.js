c290dc51d4ef221f80d4ea61c089daae66b18fc6
Erickson Teixeira <erickson.teixeira@linkka.com.br>
2018-10-15 02:38:30 -0300

    gen jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb://ericksoad:Picture*11@ds129344.mlab.com:29344/devconnector'
+    mongoURI : 'mongodb://ericksoad:Picture*11@ds129344.mlab.com:29344/devconnector',
+    secretOrKey: 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 //load user model
 const User = require('../../models/User');
@@ -68,7 +70,20 @@ router.post('/login', (req,res) => {
         bcrypt.compare(password, user.password)
         .then(isMatch => {
             if(isMatch){
-                res.json({msg: "SUCCESS"});
+                //user matched
+                const payload = { id: user.id, name: user.name, avatar: user.avatar} // create jwt payload
+
+                //sign token
+                jwt.sign(
+                    payload, 
+                    keys.secretOrKey, 
+                    { expiresIn: 3600 }, 
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: 'Bearer ' + token
+                        });
+                });
             } else {
                 return res.status(400).json({password : "Password incorrect"});
             }
