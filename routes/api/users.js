a12d440cb9f619bd1480735084638c78873d82ea
Erickson Teixeira <erickson.teixeira@linkka.com.br>
2018-10-15 13:11:24 -0300

    validate jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 //load user model
 const User = require('../../models/User');
@@ -88,6 +89,21 @@ router.post('/login', (req,res) => {
                 return res.status(400).json({password : "Password incorrect"});
             }
         });
+    });
+});
+
+//@route GET api/users/current
+//@description Return current user
+//@access private
+router.get('/current', 
+passport.authenticate('jwt', 
+{ session: false}), 
+(req, res) => {
+    //res.json({msg: 'success'});
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
     });
 });
 
