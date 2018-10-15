acfa477ddc1d72cd1fb767507b4f5b1a806ea7b0
Erickson Teixeira <erickson.teixeira@linkka.com.br>
2018-10-15 01:02:53 -0300

    Connect with mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb://ericksoad:Picture*11@ds129344.mlab.com:29344/devconnector'
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// db config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongo
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('HELOO!!') );
 
