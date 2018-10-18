a16ff1baef30148f3819551aba028a20db09fb2b
Erickson Teixeira <erickson.teixeira@linkka.com.br>
2018-10-18 02:01:59 -0300

    like and unlike route

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb://ericksoad:Picture*11@ds129344.mlab.com:29344/devconnector',
+    mongoURI : 'mongodb://erickdb:Picture*11@ds129344.mlab.com:29344/devconnector',
     secretOrKey: 'secret'
 };
