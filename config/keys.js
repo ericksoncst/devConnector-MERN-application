dbf6c8dd981d88a09156bd566cdd90768aca71ca
Erickson Teixeira <erickson.teixeira@linkka.com.br>
2018-10-23 02:42:15 -0300

    Setup Routes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb://erickdb:Picture*11@ds129344.mlab.com:29344/devconnector',
+    mongoURI : 'mongodb://erickd:sadstatue*11@ds129344.mlab.com:29344/devconnector',
     secretOrKey: 'secret'
 };
